6cc84c9b6a578d73e0e65cc37ffed74c990afc96
cathal <g00427615@atu.ie>
2025-12-13 17:00:10 +0000

    Added appointment service

---
+++ src/main/java/com/main/maincontroller/AppointmentsInterface.java
@@ -0,0 +1,33 @@
+package com.main.maincontroller;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@FeignClient(
+        name = "AppointmentsMediflow",
+        url = "localhost:8083"
+)
+
+public interface AppointmentsInterface {
+    @PostMapping("/appointments")
+    ResponseEntity<Appointment> createAppointment(@RequestBody AppointmentCreate request);
+
+    @GetMapping("/appointments/patient/{patientUserName}")
+    ResponseEntity<List<Appointment>> getByPatient(@PathVariable String patientUserName);
+
+    @GetMapping("/appointments/doctor/{doctorUserName}")
+    public ResponseEntity<List<Appointment>> getByDoctor(@PathVariable String doctorUserName);
+
+    @DeleteMapping("/appointments/{id}")
+    public ResponseEntity<Void> deleteAppointment(@PathVariable Long id);
+
+
+    @PutMapping("/appointments/{id}")
+    public ResponseEntity<Appointment> updateAppointment(
+            @PathVariable Long id,
+            @RequestBody AppointmentCreate request
+    );
+}
