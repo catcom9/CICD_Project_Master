17a7b172db09d61a1ad7ecc52b62643d33fe27d1
cathal <g00427615@atu.ie>
2026-01-09 21:30:28 +0000

    fixed typo

--- src/main/java/com/main/maincontroller/Micoservices/RecordsInterface.java
+++ src/main/java/com/main/maincontroller/Micoservices/RecordsInterface.java
@@ -3,10 +3,7 @@ package com.main.maincontroller.Micoservices;
 import com.main.maincontroller.Patient;
 import jakarta.validation.Valid;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 @FeignClient(
         name = "recordsMediflow",
@@ -14,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 )
 
 public interface RecordsInterface {
-    @GetMapping("/api/Patients/")
+    @PostMapping("/api/Patients/")
     Patient create(@RequestBody Patient person);
 
     @GetMapping("/api/Patients/{user}")
